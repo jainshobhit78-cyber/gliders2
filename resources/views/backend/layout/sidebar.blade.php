e4d2b9588dd8bda7451db90abee730547a05a97b
jainshobhit78-cyber <jain.shobhit78@gmail.com>
2026-07-18 00:59:25 +0530

    feat(admin): replace Media link with Resources menu dropdown in sidebar containing Media and Manuals and Policies

--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -90,18 +90,35 @@
                         </li>
                     @endif
 
-                    @if(auth()->guard('admin')->user()->can('media.view'))
+                    @if(auth()->guard('admin')->user()->canAny(['media.view', 'manuals_and_policies.view']))
                         <li class="sidebar-list">
-                            <a class="sidebar-link sidebar-title link-nav
-                                                                                                                                                                @if(Request::is('admin/media*')) active @endif"
-                                href="{{ url('admin/media') }}">
+                            <a class="linear-icon-link sidebar-link sidebar-title has-sub
+                                @if(Request::is('admin/media*') || Request::is('admin/vigilance*') && request('tab') == 'manuals') open @endif">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
-                                    <path
-                                        d="M20.4668 8.69379L20.7134 8.12811C21.1529 7.11947 21.9445 6.31641 22.9323 5.87708L23.6919 5.53922C24.1027 5.35653 24.1027 4.75881 23.6919 4.57612L22.9748 4.25714C21.9616 3.80651 21.1558 2.97373 20.7238 1.93083L20.4706 1.31953C20.2942 0.893489 19.7058 0.893489 19.5293 1.31953L19.2761 1.93083C18.8442 2.97373 18.0384 3.80651 17.0252 4.25714L16.308 4.57612C15.8973 4.75881 15.8973 5.35653 16.308 5.53922L17.0677 5.87708C18.0555 6.31641 18.8471 7.11947 19.2866 8.12811L19.5331 8.69379C19.7136 9.10792 20.2864 9.10792 20.4668 8.69379ZM2.9918 3H14V5H8V19H16V9H18V11H20H22V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934C2 3.44476 2.45531 3 2.9918 3ZM4 5V7H6V5H4ZM4 9V11H6V9H4ZM4 13V15H6V13H4ZM18 13V15H20V13H18ZM4 17V19H6V17H4ZM18 17V19H20V17H18Z">
-                                    </path>
+                                    <path d="M20 2H4C2.9 2 2 2.9 2 4V20C2 21.1 2.9 22 4 22H20C21.1 22 22 21.1 22 20V4C22 2.9 21.1 2 20 2ZM20 20H4V4H20V20ZM6 7H18V9H6V7ZM6 11H18V13H6V11ZM6 15H14V17H6V15Z" fill="currentColor"/>
                                 </svg>
-                                <span>Media</span>
+                                <span>Resources</span>
+                                <i class="arrow" data-feather="chevron-right"></i>
                             </a>
+                            <ul class="sidebar-submenu
+                                @if(Request::is('admin/media*') || Request::is('admin/vigilance*') && request('tab') == 'manuals') open @endif">
+                                @if(auth()->guard('admin')->user()->can('media.view'))
+                                    <li>
+                                        <a href="{{ url('admin/media') }}"
+                                            class="{{ Request::is('admin/media*') ? 'active' : '' }}">
+                                            Media
+                                        </a>
+                                    </li>
+                                @endif
+                                @if(auth()->guard('admin')->user()->can('manuals_and_policies.view'))
+                                    <li>
+                                        <a href="{{ url('admin/vigilance?tab=manuals') }}"
+                                            class="{{ Request::is('admin/vigilance*') && request('tab') == 'manuals' ? 'active' : '' }}">
+                                            Manuals and Policies
+                                        </a>
+                                    </li>
+                                @endif
+                            </ul>
                         </li>
                     @endif
 
