90541ca49309ccd97fc03f6fd331c707eca4a6db
jainshobhit78-cyber <jain.shobhit78@gmail.com>
2026-07-18 01:24:37 +0530

    feat(header): change second link in topbar from Department of Defence to Department of Defence Production

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -19,8 +19,8 @@
                             <span>Government of India</span>
                         </a>
                         <span>|</span>
-                        <a href="https://www.mod.gov.in/dod/en" target="_blank" rel="noopener noreferrer">
-                            <span>Department of Defence</span>
+                        <a href="https://www.ddpmod.gov.in/" target="_blank" rel="noopener noreferrer">
+                            <span>Department of Defence Production</span>
                         </a>
                         <span>|</span>
                         <a href="https://www.mod.gov.in/" target="_blank" rel="noopener noreferrer">
